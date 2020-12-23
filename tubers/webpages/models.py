7f67f8bfc9ddf4cd35c64dd58a6e6cec8e995951
TejaSadineni <46244941+TejaSadineni@users.noreply.github.com>
2020-12-23 12:29:08 +0530

    admin control slider

--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Slider(models.Model):
+    headline = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    button_text = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='media/slider/%Y/')
+    created_date = models.DateTimeField(auto_now_add=True)
+    url = models.URLField(null=True)
+
+    def __str__(self):
+        return self.headline
