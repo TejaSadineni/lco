5bc9cfad884aa01ffb7620ec5542cef768b30722
TejaSadineni <46244941+TejaSadineni@users.noreply.github.com>
2020-12-23 23:21:12 +0530

    youtuberapp

--- tubers/webpages/views.py
+++ tubers/webpages/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .models import Slider, Team
+from youtubers.models import Youtuber
 
 # Create your views here.
 def home(request):
-    sliders= Slider.objects.all()
-    team= Team.objects.all()
+    sliders = Slider.objects.all()
+    team = Team.objects.all()
+    featured_youtubers = Youtuber.objects.order_by('-created_date').filter(is_featured=True)
+    all_tubers = Youtuber.objects.order_by('-created_date')
     data ={
         'sliders':sliders,
-        'team':team
+        'team':team,
+        'fyoutuber':featured_youtubers,
+        'all_tubers':all_tubers,
     }
     return render(request,'webpages/home.html',data)
 
---
+++ tubers/youtubers/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from datetime import datetime
+
+from ckeditor.fields import RichTextField
+
+# Create your models here.
+class Youtuber(models.Model):
+
+    crew_choices = (
+        ('solo' , 'solo'),
+        ('small' , 'small'),
+        ('large' , 'large'),
+    )
+
+
+    camera_choices = (
+        ('canon' , 'canon'),
+        ('nikon' , 'nikon'),
+        ('sony' , 'sony'),
+        ('red', 'red'),
+        ('fuji','fuji'),
+        ('panasonic','panasonic'),
+        ('other','other'),
+    )
+
+    category_choices = (
+        ('code' , 'code'),
+        ('mobile' , 'mobile'),
+        ('Tech' , 'Tech'),
+        ('comedy', 'comedy'),
+        ('film_making','film_making'),
+        ('cooking','cooking'),
+        ('gaming','gaming'),
+        ('vlog','vlog'),
+        ('news','news'),
+        ('adult','adult'),
+        ('cartoon','cartoon'),
+    )
+
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()
+    photo = models.ImageField(upload_to='media/ytubers/%Y/%m')
+    video_url = models.CharField(max_length=255)
+    description = RichTextField()
+    city = models.CharField(max_length=255)
+    age = models.IntegerField()
+    height = models.IntegerField()
+    crew = models.CharField(max_length=255,choices=crew_choices)
+    camera_type = models.CharField(max_length=255,choices=camera_choices)
+    subs_count = models.CharField(max_length=255)
+    category = models.CharField(max_length=255,choices=category_choices)
+    is_featured = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+
