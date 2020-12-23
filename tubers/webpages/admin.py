17369e7812c6df49d2e35b5901a33270719a1723
TejaSadineni <46244941+TejaSadineni@users.noreply.github.com>
2020-12-23 14:22:54 +0530

    admin team costomize

--- tubers/webpages/admin.py
+++ tubers/webpages/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import Slider , Team
+from django.utils.html import format_html
 # Register your models here.
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name','role','created_date')
+
+    def myphoto(self,object):
+        return format_html('<img src="{}" width="40"  />'.format(object.photo.url))
+
+
+    list_display = ('id','myphoto','first_name','role','created_date')
     list_display_links = ('id','first_name')
     list_filter = ('role',)
     search_fields = ('first_name',)
